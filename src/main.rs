243d83c8112fc4710c7cf84e7d1174f379410f27
Luca Lewin <luca.lewin23@gmail.com>
2023-03-31 00:36:50 +0200

    add test

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,34 @@
-use std::marker::PhantomData;
+// use std::marker::PhantomData;
 
-use measurement::test::{Quantity, Length, Time, Meter, Second, Velocity, MeterPerSecond};
+// use measurement::test::{Quantity, Length, Time, Meter, Second, Velocity, MeterPerSecond};
+
+use measurement::si::{Length, Time, Velocity, Mass};
 
 fn main() {
-    let length = Quantity::<Length<Meter>> { value: 15.0, d: PhantomData::default() };
-    let time = Quantity::<Time<Second>> { value: 5.0, d: PhantomData::default() };
+
+    // let length = Quantity::<Length<Meter>> { value: 15.0, d: PhantomData::default() };
+    // let time = Quantity::<Time<Second>> { value: 5.0, d: PhantomData::default() };
     
-    let acceleration = length / time;
+    // let acceleration = length / time;
 
-    println!("acc: {}", acceleration.value);
+    // println!("acc: {}", acceleration.value);
 
-    println!("size: {}", core::mem::size_of::<Quantity<Velocity<MeterPerSecond>>>());
+    // println!("size: {}", core::mem::size_of::<Quantity<Velocity<MeterPerSecond>>>());
+    test()
+}
+
+fn test() {
+    let length = Length::new(20.0);
+    let time = Time::new(5.0);
+    let velocity = speed(length, time);
+
+    let acc = velocity / Time::new(2.0);
+
+    let force = Mass::new(30.0) * acc;
+
+    println!("force: {}", force.0);
+}
+
+fn speed(length: Length, time: Time) -> Velocity {
+    length / time
 }
