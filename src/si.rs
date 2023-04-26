b7845d981d6e6986229601ae2128424eb4d185c3
Luca Lewin <luca.lewin23@gmail.com>
2023-04-26 16:32:59 +0200

    add more quantities

--- src/si.rs
+++ src/si.rs
@@ -1,8 +1,13 @@
 use crate::{Quantity, Dimension};
 
-pub type Length = Quantity<Dimension<1, 0, 0, 0, 0, 0, 0>>;
-pub type Mass = Quantity<Dimension<0, 1, 0, 0, 0, 0, 0>>;
-pub type Time = Quantity<Dimension<0, 0, 1, 0, 0, 0, 0>>;
+pub type Length                  = Quantity<Dimension<1, 0, 0, 0, 0, 0, 0>>;
+pub type Mass                    = Quantity<Dimension<0, 1, 0, 0, 0, 0, 0>>;
+pub type Time                    = Quantity<Dimension<0, 0, 1, 0, 0, 0, 0>>;
+pub type ElectricalCurrent       = Quantity<Dimension<0, 0, 0, 1, 0, 0, 0>>;
+pub type ThermodynamicTemperatur = Quantity<Dimension<0, 0, 0, 0, 1, 0, 0>>;
+pub type AmountOfSubstance       = Quantity<Dimension<0, 0, 0, 0, 0, 1, 0>>;
+pub type LuminousIntensity       = Quantity<Dimension<0, 0, 0, 0, 0, 0, 1>>;
 
-pub type Velocity = Quantity<Dimension<1, 0, -1, 0, 0, 0, 0>>;
+pub type Velocity     = Quantity<Dimension<1, 0, -1, 0, 0, 0, 0>>;
 pub type Acceleration = Quantity<Dimension<1, 0, -2, 0, 0, 0, 0>>;
+pub type Force        = Quantity<Dimension<1, 1, -2, 0, 0, 0, 0>>;
